5321e654e83728b3d90ed6e49493a41f932411e9
Ameya <ameyasawant5@gmail.com>
2022-11-16 22:55:34 +0530

    Added minor changes

--- uid-service/src/main/java/org/DUIDServiceMain.java
+++ uid-service/src/main/java/org/DUIDServiceMain.java
@@ -1,11 +1,13 @@
 package org;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"org.duid"})
+@ComponentScan({"org.duid", "org.data"})
+@Log4j2
 public class DUIDServiceMain {
     public static void main(String[] args) {
         SpringApplication.run(DUIDServiceMain.class, args);
